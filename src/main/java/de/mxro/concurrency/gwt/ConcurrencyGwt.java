1b175a2c61ff7288e2fb90747ed21936111acaa0
mxro <mxro@nowhere.com>
2014-09-22 08:26:03 +1000

    Refactoring concurrency module and adding module with GWT concurrency Autocommit Mon Sep 22 08:26:03 EST 2014

--- src/main/java/de/mxro/concurrency/gwt/ConcurrencyGwt.java
+++ src/main/java/de/mxro/concurrency/gwt/ConcurrencyGwt.java
@@ -1,11 +1,17 @@
 package de.mxro.concurrency.gwt;
 
 import de.mxro.concurrency.Concurrency;
+import de.mxro.concurrency.gwt.internal.GwtConcurrencyFactory;
+import de.mxro.factories.Factory;
 
 public class ConcurrencyGwt {
 
     public static Concurrency create() {
         return new GwtConcurrencyImpl();
+    }
+
+    public static Factory<?, ?, ?> createFactory() {
+        return new GwtConcurrencyFactory();
     }
 
 }
--- src/main/java/de/mxro/concurrency/gwt/internal/GwtConcurrencyFactory.java
+++ src/main/java/de/mxro/concurrency/gwt/internal/GwtConcurrencyFactory.java
@@ -1,5 +1,22 @@
 package de.mxro.concurrency.gwt.internal;
 
-public class GwtConcurrencyFactory {
+import de.mxro.concurrency.Concurrency;
+import de.mxro.concurrency.configuration.ConcurrencyConfiguration;
+import de.mxro.concurrency.gwt.ConcurrencyGwt;
+import de.mxro.factories.Configuration;
+import de.mxro.factories.Dependencies;
+import de.mxro.factories.Factory;
+
+public class GwtConcurrencyFactory implements Factory<Concurrency, ConcurrencyConfiguration, Dependencies> {
+
+    @Override
+    public boolean canInstantiate(final Configuration conf) {
+        return conf instanceof ConcurrencyConfiguration;
+    }
+
+    @Override
+    public Concurrency create(final ConcurrencyConfiguration conf, final Dependencies dependencies) {
+        return ConcurrencyGwt.create();
+    }
 
 }
